904442aeb1b0aea9498cfe215a4959396ae6768e
Shivsurya <kshitijbagal@gmail.com>
2025-02-23 13:25:26 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,19 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Library/',
+  server: {
+    headers: {
+      "Content-Security-Policy": "connect-src 'self' data:;",
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Resource-Policy": "same-site",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      "Access-Control-Allow-Credentials": "true",
+    }
+  },
+  build: {
+    outDir: 'dist', // Build output folder
+  },
 })
